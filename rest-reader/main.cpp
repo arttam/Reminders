c1c12ffd60443587534d471b460ad7bef23320a0
Arturs Tamanis <arturs.tamanis@gmail.com>
2016-12-18 22:32:20 +0000

    Work in progress

--- rest-reader/main.cpp
+++ rest-reader/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <boost/asio.hpp>
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    try {
+        using boost::asio::ip::tcp;
+
+        boost::asio::io_service io_service;
+        tcp::iostream socketstream(tcp::resolver::query{"localhost", "9000"});
+        std::string respStr;
+
+        auto execCmd = [&socketstream, &respStr](std::string cmd) -> void
+        {
+            socketstream << cmd;
+            while(std::getline(socketstream, respStr) && respStr != "\r") {
+                std::cout << respStr << std::endl;
+            }
+            socketstream.flush();
+        };
+
+        execCmd("/get");
+        execCmd("/delete/ZZ");
+        execCmd("/get");
+        execCmd("/commit");
+
+        /*
+        socketstream << "/get";
+        while(std::getline(socketstream, respStr) && respStr != "\r") {
+            std::cout << respStr << std::endl;
+        }
+        socketstream.flush();
+
+        socketstream << "/delete/ZZ";
+        while(std::getline(socketstream, respStr) && respStr != "\r") {
+            std::cout << respStr << std::endl;
+        }
+        socketstream.flush();
+
+        */
+        socketstream.close();
+    }
+    catch(std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Done ..." << std::endl;
     return 0;
 }
